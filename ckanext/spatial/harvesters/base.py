9e1b27f7dd4afda6d63fd428c6b5122878018227
carlo cancellieri <ccancellieri@hotmail.com>
2021-10-28 13:39:48 +0200

    Lineage is missing on 19139 ISO base implementation #263
    Lineage is missing on 19139 ISO base implementation #263
    
    Lineage is missing on 19139 ISO base implementation #263

--- ckanext/spatial/harvesters/base.py
+++ ckanext/spatial/harvesters/base.py
@@ -471,6 +471,7 @@ class SpatialHarvester(HarvesterBase):
                         })
                     package_dict['resources'].append(resource)
 
+        extras['resources'] = iso_values.get('lineage')
 
         # Add default_extras from config
         default_extras = self.source_config.get('default_extras',{})
