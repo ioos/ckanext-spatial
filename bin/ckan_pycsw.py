d1d6f3f92f85eb2509f77263ab7d03b1b432da41
Benjamin Adams <benjamin.adams@tetratech.com>
2024-02-29 10:31:39 -0500

    Remove extraneous open block/code

--- bin/ckan_pycsw.py
+++ bin/ckan_pycsw.py
@@ -58,9 +58,6 @@ def set_keywords(pycsw_config_file, pycsw_config, ckan_url, limit=20):
     log.info("Setting tags in pycsw configuration file %s", pycsw_config_file)
     pycsw_config.set("metadata:main", "identification_keywords", keywords)
     with open(pycsw_config_file, "wb") as configfile:
-    log.info('Setting tags in pycsw configuration file %s', pycsw_config_file)
-    pycsw_config.set('metadata:main', 'identification_keywords', keywords)
-    with codecs.open(pycsw_config_file, 'wb', 'utf-8') as configfile:
         pycsw_config.write(configfile)
 
 
